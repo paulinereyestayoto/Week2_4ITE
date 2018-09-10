e0812ffa760d39a871185582c3973d9e15c13307
Pauline Tayoto <paulinetayoto@gmail.com>
2018-09-10 17:12:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/acer/week2_4ite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.acer.week2_4ite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
